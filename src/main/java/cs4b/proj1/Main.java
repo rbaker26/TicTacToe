9d4db8c9368442822145df27ac1a48294633cf07
Keane Kaiser <kkaiser3@saddleback.edu>
2019-01-24 20:36:29 -0800

    added gradle build sys

---
+++ src/main/java/cs4b/proj1/Main.java
@@ -0,0 +1,26 @@
+package cs4b.proj1;
+
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+		URL url = getClass().getResource("sample.fxml");
+		System.out.println(url);
+        Parent root = FXMLLoader.load(url);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
