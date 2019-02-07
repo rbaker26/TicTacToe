072f97147b59061a9e158945c0f3e3685412b5ad
kkaiser87 <kaiser.keane@gmail.com>
2019-02-07 00:45:27 -0800

    Board GUI finished with toggeling of tokens

--- src/main/java/cs4b/proj1/BoardGUI.java
+++ src/main/java/cs4b/proj1/BoardGUI.java
@@ -1,13 +1,18 @@
 package cs4b.proj1;
 
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.TilePane;
-
 import java.io.FileInputStream;
 
-public class BoardGUI extends TilePane {
+/*
+ * This board is a simple 3 x 3 grid set up to play tic-tac-toe using a javafx gridpane
+ * The constructor iterates through each cell of the gridpane placing a pane with an ImageView.
+ * Each pane is set up with an on click event to handle the toggling the tokens in the space
+ */
+public class BoardGUI extends GridPane {
     private final int MAX_SIZE = 3;
     private Image xImg;
     private Image oImg;
@@ -23,19 +28,33 @@ public class BoardGUI extends TilePane {
             System.out.println("File not found");
         }
 
-
-        try {
-            for (int row = 0; row < MAX_SIZE; row++) {
-                for (int col = 0; col < MAX_SIZE; col++) {
-
-//                    this.getChildren().add();
-
-                }
+        for (int row = 0; row < MAX_SIZE; row++) {
+            for (int col = 0; col < MAX_SIZE; col++) {
+                Pane space = new Pane();
+                ImageView token = new ImageView();
+                token.setImage(xImg);
+                token.setPreserveRatio(true);
+                token.setFitHeight(100);
+                space.getChildren().add(token);
+                space.setOnMouseClicked(event -> {
+                    this.toggleToken((Node)event.getSource());
+//                    GridPane.getRowIndex((Node)event.getSource());
+//                    GridPane.getColumnIndex((Node)event.getSource());
+                });
+                this.add(space, col, row);
             }
         }
-        catch(Exception ex) {
-            System.out.println("Fuck");
-        }
+        this.setGridLinesVisible(true);
+    }
 
+    public void toggleToken(Node node) {
+        ImageView token = (ImageView)((Pane)node).getChildren().get(0);
+        if(token.getImage().equals(emptyImg)) {
+            token.setImage(xImg);
+        } else if(token.getImage().equals(xImg)) {
+            token.setImage(oImg);
+        } else {
+            token.setImage(emptyImg);
+        }
     }
 }
--- src/main/java/cs4b/proj1/Main.java
+++ src/main/java/cs4b/proj1/Main.java
@@ -2,21 +2,23 @@ package cs4b.proj1;
 
 import java.net.URL;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-		URL url = getClass().getResource("board.fxml");
-		System.out.println(url);
 		BoardGUI board = new BoardGUI();
-        Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        board.requestFocus();
+        primaryStage.setScene(new Scene(board, 300, 300));
         primaryStage.show();
     }
 
