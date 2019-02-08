b8f94aef93d75ef33f71e10394654c3c93f8df4f
Autofire <autofire555@gmail.com>
2019-02-08 14:49:26 -0800

    Wrote some more tests.

--- src/test/java/cs4b/proj1/observer/SubjectAssistantTest.java
+++ src/test/java/cs4b/proj1/observer/SubjectAssistantTest.java
@@ -29,7 +29,7 @@ public class SubjectAssistantTest {
     }
 
     @Test
-    public void testAssistant() {
+    public void testSubscribe() {
         SubjectAssistant<Modes> sa = new SubjectAssistant<>();
         TestObs o1 = new TestObs(0);
         TestObs o2 = new TestObs(2);
@@ -58,5 +58,83 @@ public class SubjectAssistantTest {
         assertEquals(o1.val, 30);
         assertEquals(o2.val, 22);
         assertEquals(o3.val, -4);
+    }
+
+    @Test
+    public void testDuplicateSubscribe() {
+        SubjectAssistant<Modes> sa = new SubjectAssistant<>();
+        TestObs o1 = new TestObs(0);
+
+        sa.subscribe(o1, Modes.mode1);
+        sa.subscribe(o1, Modes.mode1);
+
+        sa.triggerUpdate(Modes.mode1, 10);
+        assertEquals(o1.val, 10);
+    }
+
+    @Test
+    public void testUnsubscribe() {
+        SubjectAssistant<Modes> sa = new SubjectAssistant<>();
+        TestObs o1 = new TestObs(0);
+        TestObs o2 = new TestObs(2);
+        TestObs o3 = new TestObs(-4);
+
+        sa.subscribe(o1, Modes.mode1);
+        sa.subscribe(o2, Modes.mode2);
+        sa.subscribe(o3, Modes.mode1);
+        sa.subscribe(o3, Modes.mode2);
+
+        sa.triggerUpdate(Modes.mode1, 5);
+        assertEquals(o1.val, 5);
+        assertEquals(o2.val, 2);
+        assertEquals(o3.val, 1);
+
+        sa.unsubscribe(o1, Modes.mode2);    // Note that o1 is not subscribed to mode2!
+        sa.triggerUpdate(Modes.mode2, 5);
+        assertEquals(o1.val, 5);
+        assertEquals(o2.val, 7);
+        assertEquals(o3.val, 6);
+
+        sa.unsubscribe(o1, Modes.mode1);
+        sa.triggerUpdate(Modes.mode1, 1);
+        assertEquals(o1.val, 5);
+        assertEquals(o2.val, 7);
+        assertEquals(o3.val, 7);
+
+        sa.unsubscribe(o3, Modes.mode1);
+        sa.triggerUpdate(Modes.mode1, 1);
+        assertEquals(o1.val, 5);
+        assertEquals(o2.val, 7);
+        assertEquals(o3.val, 7);
+    }
+
+    @Test
+    public void testUnsubscribeAll() {
+        SubjectAssistant<Modes> sa = new SubjectAssistant<>();
+        TestObs o1 = new TestObs(0);
+        TestObs o2 = new TestObs(2);
+        TestObs o3 = new TestObs(-4);
+
+        sa.subscribe(o1, Modes.mode1);
+        sa.subscribe(o2, Modes.mode2);
+        sa.subscribe(o3, Modes.mode1);
+        sa.subscribe(o3, Modes.mode2);
+
+        sa.triggerUpdate(Modes.mode1, 5);
+        assertEquals(o1.val, 5);
+        assertEquals(o2.val, 2);
+        assertEquals(o3.val, 1);
+
+        sa.unsubscribeAll(o3);
+        sa.triggerUpdate(Modes.mode2, -2);
+        assertEquals(o1.val, 5);
+        assertEquals(o2.val, 0);
+        assertEquals(o3.val, 1);
+
+        sa.unsubscribeAll(o1);
+        sa.triggerUpdate(Modes.mode1, 1);
+        assertEquals(o1.val, 5);
+        assertEquals(o2.val, 0);
+        assertEquals(o3.val, 1);
     }
 }
