25da8723606b64a82eb4911a70f79bc471075351
naomidonato <36245167+naomidonato@users.noreply.github.com>
2019-02-12 10:19:44 -0800

    first

--- src/main/java/cs4b/proj1/Main.java
+++ src/main/java/cs4b/proj1/Main.java
@@ -4,26 +4,52 @@ import java.net.URL;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-		BoardGUI board = new BoardGUI();
-        primaryStage.setTitle("Hello World");
-        board.requestFocus();
-        primaryStage.setScene(new Scene(board, 300, 300));
-        primaryStage.show();
-    }
+
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+          BoardGUI board = new BoardGUI();
+      //  primaryStage.setTitle("Hello World");
+          board.requestFocus();
+
+
+        //  primaryStage.setScene(new Scene(root, 500, 400));
+        //  primaryStage.show();
+
+
+
+
+
+       // StackPane root2 = new StackPane();
+        root.setId("pane");
+        Scene scene = new Scene(root, 500, 400);
+
+        scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+
+
 }
+
+
