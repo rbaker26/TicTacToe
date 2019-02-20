4e1672e1a9cd779caff49413fd08700696017df4
naomidonato <36245167+naomidonato@users.noreply.github.com>
2019-02-19 21:56:54 -0800

    Controller.java now implements sample.fxml

--- src/main/java/cs4b/proj1/Controller.java
+++ src/main/java/cs4b/proj1/Controller.java
@@ -3,10 +3,14 @@ package cs4b.proj1;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-
+import javafx.event.ActionEvent;
 
 public class Controller  {
 
+    @FXML
+    private void button1Click() {
+
+    }
 
 
 
--- src/main/java/cs4b/proj1/Main.java
+++ src/main/java/cs4b/proj1/Main.java
@@ -1,29 +1,13 @@
 package cs4b.proj1;
 
-import java.awt.*;
-import java.net.URL;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
 public class Main extends Application {
-
-
-
 
     public static void main(String[] args) {
         launch(args);
@@ -55,7 +39,7 @@ public class Main extends Application {
        // root.setId("pane");
         Scene scene = new Scene(root, 360, 450);
 
-        scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+      //  scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
 
         //primaryStage.setTitle("Hello World");
         //primaryStage.setScene(new Scene(root, 389, 450));
@@ -67,52 +51,7 @@ public class Main extends Application {
         primaryStage.show();
 
 
-        /*******
-         *
-        //WHAT FOLLOWS IS A SAMPLE WHICH SHOWS HOW TO SET UP THE GAME BOARD
 
-        // This will just set up a basic, AI v. AI mode. In the end, this setup will be handled
-        // by Naomi's main menu.
-		BoardGUI board = new BoardGUI();
-
-		// Actually construct our players. This is where we can choose which ones
-        // are humans and which ones are AI.
-        Player p1 = new Player('X', "Human1", new HPCLocal());
-        //Player p1 = new Player('X', "AI1", new NPCEasy());
-
-        //Player p2 = new Player('O', "Human2", new HPCLocal());
-        Player p2 = new Player('O', "AI2", new NPCHard('X', 'O'));
-
-
-        // Construct the game, using the players we've constructed up above.
-        Game game = new Game(p1, p2);
-
-        // The board must be able to let the player behavior know what has been clicked.
-        // This is only important for human players, though, as AI can figure it out
-        // on their own.
-        if(p1.getPb() instanceof HPCLocal) {
-            board.addSubscriber((HPCLocal) p1.getPb());
-        }
-
-        if(p2.getPb() instanceof HPCLocal) {
-            board.addSubscriber((HPCLocal) p2.getPb());
-        }
-
-
-        // The game engine needs to let the board know when there's an update.
-        game.addSubscriber(board);
-
-
-        // What follows is UI setup stuff.
-        primaryStage.setTitle("Hello World");
-        board.requestFocus();
-        primaryStage.setScene(new Scene(board, 300, 300));
-        primaryStage.show();
-
-        // Now that EVERYTHING is set up, the game can begin!
-        game.startGame();
-
-        */
     }
 }
 
