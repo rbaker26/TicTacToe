e5769b0d6f59459cd6211ff1de7f11087052d11a
naomidonato <36245167+naomidonato@users.noreply.github.com>
2019-02-20 12:18:43 -0800

    Controller class now is connected with sample.fxml

--- src/main/java/cs4b/proj1/Controller.java
+++ src/main/java/cs4b/proj1/Controller.java
@@ -7,8 +7,32 @@ import javafx.event.ActionEvent;
 
 public class Controller  {
 
+    //On Mouse Click Options:
+
+    //If the player wants to compete against another player
+    //button1Click() will be incorporated.
     @FXML
     private void button1Click() {
+
+        System.out.println("HELLO THERE");
+
+    }
+
+    //If the player wants to compete against the AI in an easy level,
+    //button2Click() will be incorporated.
+    @FXML
+    private void button2Click() {
+
+        System.out.println("SUP");
+
+    }
+
+    //If the player wants to compete against the AI in a hard level,
+    //button3Click() will be incorporated.
+    @FXML
+    private void button3Click() {
+
+        System.out.println("HEY");
 
     }
 
