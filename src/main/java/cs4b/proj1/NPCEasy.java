46df6e42f70e0c76f3b9bf75ca53d03538e01e5d
Autofire <autofire555@gmail.com>
2019-02-20 17:46:37 -0800

    Fixed bug that caused EasyNPC to freeze up.
    Also, enhanced the GameTest to not miss stuff.

--- src/main/java/cs4b/proj1/NPCEasy.java
+++ src/main/java/cs4b/proj1/NPCEasy.java
@@ -69,15 +69,14 @@ public class NPCEasy implements PlayerBehavior {
     public void getMove(Board b, char token) {
 
         Pair<Integer, Integer> result = null;
-        char[][] boardArray = b.getBoardArray();
-
         ArrayList<Pair<Integer, Integer>> emptySpaces = new ArrayList<>();
 
+
         // Get all empty spaces
-        for(int row = 0; row < boardArray.length; row++) {
-            for(int col = 0; col < boardArray[row].length; col++) {
-                if(boardArray[row][col] == b.DEFAULT_VALUE) {
-                    emptySpaces.add(new Pair<>(row, col));
+        for(int x = 0; x < b.BOARD_SIZE_X; x++) {
+            for(int y = 0; y < b.BOARD_SIZE_Y; y++) {
+                if(b.getPos(x,y) == b.DEFAULT_VALUE) {
+                    emptySpaces.add(new Pair<>(x, y));
                 }
             }
         }
--- src/test/java/cs4b/proj1/GameTest.java
+++ src/test/java/cs4b/proj1/GameTest.java
@@ -26,33 +26,38 @@ public class GameTest {
         //
         // Because of that, if NPCEasy were to break, so could this. But
         // there's no time for caution!
-
-        Player p1 = new Player('X', "AI1", new NPCEasy());
-        Player p2 = new Player('O', "AI2", new NPCEasy());
-
-        Game g = new Game(p1, p2);
-
-        EventContainer<Game.MoveInfo> obs = new EventContainer<>(Game.MoveInfo.class);
-        g.addSubscriber(obs);
-
-        g.startGame();
-
-        // Given that we have AI, they should finish the game the moment
-        // we call the "startGame" method. Thus, it's a problem if the
-        // game isn't over by now.
-        assertTrue(g.gameOver());
-
-
-        // Okay, if that worked, then we'll make sure the turn order is
-        // working correctly.
         //
-        // Before the game began, nextPlayer was player1. It should
-        // alternate every turn.
-        Player prevPlayer = p1;
+        // Need to repeat because of randomness.
+        final int REPEAT_COUNT = 1000;
 
-        for(Game.MoveInfo info : obs.getAllEvents()) {
-            assertNotEquals(prevPlayer, info.getNextPlayer());
-            prevPlayer = info.getNextPlayer();
+        for(int i = 0; i < REPEAT_COUNT; i++) {
+
+            Player p1 = new Player('X', "AI1", new NPCEasy());
+            Player p2 = new Player('O', "AI2", new NPCEasy());
+
+            Game g = new Game(p1, p2);
+
+            EventContainer<Game.MoveInfo> obs = new EventContainer<>(Game.MoveInfo.class);
+            g.addSubscriber(obs);
+
+            g.startGame();
+
+            // Given that we have AI, they should finish the game the moment
+            // we call the "startGame" method. Thus, it's a problem if the
+            // game isn't over by now.
+            assertTrue(g.gameOver());
+
+            // Okay, if that worked, then we'll make sure the turn order is
+            // working correctly.
+            //
+            // Before the game began, nextPlayer was player1. It should
+            // alternate every turn.
+            Player prevPlayer = p1;
+
+            for (Game.MoveInfo info : obs.getAllEvents()) {
+                assertNotEquals(prevPlayer, info.getNextPlayer());
+                prevPlayer = info.getNextPlayer();
+            }
         }
     }
 
