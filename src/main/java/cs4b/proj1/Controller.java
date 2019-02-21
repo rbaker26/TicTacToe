f31970b7236693659b2f0abddf4e2e0c330d5c9b
naomidonato <36245167+naomidonato@users.noreply.github.com>
2019-02-20 19:07:26 -0800

    Controller class has signals

--- src/main/java/cs4b/proj1/Controller.java
+++ src/main/java/cs4b/proj1/Controller.java
@@ -1,20 +1,114 @@
 package cs4b.proj1;
 
+import cs4b.proj1.observer.IObserver;
+import cs4b.proj1.observer.ISubject;
+import cs4b.proj1.observer.SubjectAssistant;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 
-public class Controller  {
+public class Controller implements ISubject {
+
+    //region ISubject *************************************************************
+
+    private SubjectAssistant subjAssist;
+
+    /**
+     * Subscribes the given observer, causing its update function to be called
+     * for the given event. As there can be a variety of modes, subjects are
+     * expected to implement some kind of object (e.g. an enum) to allow
+     * subscribers to select what kind of events they are interested in.
+     * <p>
+     * If an observer attempts to addSubscriber itself more than once, the first
+     * subscription should be replaced. (Unless they are with differenct
+     * modes, of course.)
+     *
+     * @param observer The observer which will be subscribed.
+     * @author Daniel Edwards
+     */
+    @Override
+    public void addSubscriber(IObserver observer) {
+        if(subjAssist == null) {
+            subjAssist = new SubjectAssistant();
+        }
+
+        subjAssist.addSubscriber(observer);
+    }
+
+    /**
+     * Unsubscribes the given observer so that they will no longer receive
+     * updates for the given event. Nothing should happen if the observer
+     * isn't subscribed.
+     *
+     * @param observer Observer to be unsubscribed.
+     * @author Daniel Edwards
+     */
+    @Override
+    public void removeSubscriber(IObserver observer) {
+        if(subjAssist == null) {
+            subjAssist = new SubjectAssistant();
+        }
+
+        subjAssist.removeSubscriber(observer);
+    }
+
+    private void triggerUpdate(Object event) {
+        if(subjAssist == null) {
+            subjAssist = new SubjectAssistant();
+        }
+
+        subjAssist.triggerUpdate(event);
+    }
+
+    //endregion ISubject ***********************************************************
+
 
     //On Mouse Click Options:
 
     //If the player wants to compete against another player
     //button1Click() will be incorporated.
+
+    @FXML
+    private TextField player1txt;
+
+    @FXML
+    private TextField player2txt;
+
     @FXML
     private void button1Click() {
 
+        //Daniel signal- main captures, then it will call the board ui.
+
+
+        // Sets up game engine stuff
         System.out.println("HELLO THERE");
+        //move this to text editor function:
+        String player1 = player1txt.getText();
+        String player2 = player2txt.getText();
+
+        if(player1 == "") {
+            player1 = "Player 1";
+        }
+
+        if(player2 == "") {
+            player2 = "Player 2";
+        }
+
+
+
+        Player p1 = new Player('X', player1, new HPCLocal());
+        Player p2 = new Player('O', player2, new HPCLocal());
+
+        Game game = new Game(p1, p2);
+
+
+        subjAssist.triggerUpdate(game);
+
+
+
 
     }
 
@@ -31,6 +125,8 @@ public class Controller  {
     //button3Click() will be incorporated.
     @FXML
     private void button3Click() {
+
+
 
         System.out.println("HEY");
 
--- src/main/java/cs4b/proj1/Main.java
+++ src/main/java/cs4b/proj1/Main.java
@@ -1,5 +1,6 @@
 package cs4b.proj1;
 
+import cs4b.proj1.observer.IObserver;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,48 +8,60 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class Main extends Application {
+public class Main extends Application implements IObserver {
+
+    Controller controlObject = new Controller();
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
+    public void update(Object eventInfo) {
+
+        if(eventInfo instanceof Game) {
+
+            Game game = (Game)eventInfo;
+
+            // This sets up the UI stuff
+            BoardGUI board = new BoardGUI();
+
+            board.requestFocus();
+
+            if(p1.getPb() instanceof HPCLocal) {
+                board.addSubscriber((HPCLocal) p1.getPb());
+            }
+
+            if(p2.getPb() instanceof HPCLocal) {
+                board.addSubscriber((HPCLocal) p2.getPb());
+            }
+
+            // Last function to call when everything is ready
+            game.startGame();
+        }
+
+
+
+    }
+
+    @Override
     public void start(Stage primaryStage) throws Exception{
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-          BoardGUI board = new BoardGUI();
 
-          board.requestFocus();
           
 
-
-       // ImageView imgObject = new ImageView("img\\gameImage.jpg");
-
-       // imgObject.fitWidthProperty().bind(primaryStage.widthProperty());
-
-       // paneObject.setCenterShape(imgObject);
-
-
-       // Scene scene = new Scene(paneObject);
-
-       // primaryStage.setScene(scene);
-        //primaryStage.show();
-
-       // StackPane root2 = new StackPane();
-       // root.setId("pane");
         Scene scene = new Scene(root, 360, 450);
 
-      //  scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
-
-        //primaryStage.setTitle("Hello World");
-        //primaryStage.setScene(new Scene(root, 389, 450));
 
         primaryStage.setResizable(false);
 
         primaryStage.setScene(scene);
 
         primaryStage.show();
+
+
+
 
 
 
